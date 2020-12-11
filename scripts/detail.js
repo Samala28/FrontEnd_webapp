b3022fc9376df22d18c5b5f3698b61e18ed47795
Samala28 <samalayashwanth.1997@gmail.com>
2020-12-11 22:35:04 +0530

    checkpoint12

---
+++ scripts/detail.js
@@ -0,0 +1,76 @@
+let headerTemplate =` <a href="D:\Upgrad Projects\Hotel_Booking_Website_Starter_Code\index.html" ><img src = "D:\Upgrad Projects\Hotel_Booking_Website_Starter_Code\assests\images\logo.png" class="logo" /></a>
+<span ><a href ="D:\Upgrad Projects\Hotel_Booking_Website_Starter_Code\login.html" target="_self" id="login"><button class="btn btn-light" data-toggle="modal" data-target="#loginModal" id="login-button" type="button" >LOGIN</button></a></span>
+<!-- Modal -->
+<div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="loginModalLabel" aria-hidden="true">
+<div class="modal-dialog" role="document">
+   <div class="modal-content">
+       <div class="modal-header">
+           <h5 class="modal-title" id="loginModalLabel">Please Login</h5>
+           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+               <span aria-hidden="true">&times;</span>
+             </button>
+       </div>
+       <div class="modal-body">
+           <form>
+               <div>
+               <div class="form-group">
+                   <label for="username">Username:</label>
+                   <input type="text" id="username" name="username" placeholder="Enter Username" required/>
+                   <br/>
+               </div>
+               <div class="form-group">
+                   <label for="password">Password:</label>
+                   <input type="password" name="password" id="password" placeholder="Enter Password" required/><br/>
+               </div>  
+               </div>
+           </form>
+   </div>
+   <div class="modal-footer" style="justify-content: center;">
+       <div><button type="login" id="primary" class="btn btn-primary">Login</button></div>
+   </div>
+</div>
+</div>
+</div>`;
+
+document.getElementById('header').innerHTML=headerTemplate;
+
+let footerTemplate=` <div><a href="D:\Upgrad Projects\Hotel_Booking_Website_Starter_Code\contact.html" id="contact"><button class="btn btn-primary" type="button" data-toggle="modal" data-target="#submitModal" id="submit-button">Contact Us</button></a></div>
+<!-- Modal -->
+<div class="modal fade" id="submitModal" tabindex="-1" role="dialog" aria-labelledby="loginModalLabel" aria-hidden="true">
+<div class="modal-dialog" role="document">
+<div class="modal-content">
+<div class="modal-header">
+  <h5 class="modal-title" id="loginModalLabel">Get in touch</h5>
+  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>
+<div class="modal-body">
+
+            <div class="form-contact">
+                <p>Thank you for reaching out!!!</p>
+                <p>Please enter your email and we will get back to you.</p>
+                <form action="D:\Upgrad Projects\Hotel_Booking_Website_Starter_Code\index.html" method="GET" autocomplete="off">
+                <label for="email">Email:</label>
+                <input type="email" name="email" id="email" placeholder="Enter your email id"/><br/>
+            </form>
+            </div>
+            
+       
+</div>
+<div class="modal-footer">
+    <div id="contact-btn">
+        <button type="login" class="btn btn-primary">Submit</button>
+        </div>
+</div>
+</div>
+</div>
+</div>
+<div class="copy">&copy;2020 ROOM SEARCH PVT.LTD.</div>
+                <span id="social-media">
+                <a href="https://www.facebook.com" target="_blank"><img src="D:\Upgrad Projects\Hotel_Booking_Website_Starter_Code\assests\images\facebook.png" class="footer" /></a>
+                <a href="https://www.instagram.com" target="_blank"><img src="D:\Upgrad Projects\Hotel_Booking_Website_Starter_Code\assests\images\instagram.png" class="footer" /></a>
+                <a href="https://twitter.com" target="_blank"><img src="D:\Upgrad Projects\Hotel_Booking_Website_Starter_Code\assests\images\twitter.png" class="footer" /></a>
+                </span>`;
+
+document.getElementById('footer-color').innerHTML=footerTemplate;
